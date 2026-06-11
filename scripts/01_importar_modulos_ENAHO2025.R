1ed6b35df29cf4d2d2205a95fc7b677ab5a02234
guillermocoronado <guillermo.coronado@pucp.edu.pe>
2026-06-11 17:59:28 -0500

    Importamos base de datos, hicimos joins y exportamos base limpia a parquet

--- scripts/01_importar_modulos_ENAHO2025.R
+++ scripts/01_importar_modulos_ENAHO2025.R
@@ -11,6 +11,17 @@ library(rio)
 library(tidyverse)
 library(readr)
 library(janitor)
+renv::snapshot()
 
 #2.Importar---------------
-mod100 <- import("datos/crudos/Enaho01-2025-100.csv")
+
+#Datos2025
+hogar2025 <- import("datos/crudos/Enaho01-2025-100.csv")
+carac2025 <- import("datos/crudos/Enaho01-2025-200.csv")
+salud2025 <- import("datos/crudos/Enaho01-2025-300.csv")
+educacion2025 <- import("datos/crudos/Enaho01-2025-400.csv")
+empleo2025 <- import("datos/crudos/Enaho01-2025-500.csv")
+confianza2025- import("datos/crudos/Enaho01B-2025-1.csv") 
+
+
+
